e8b0efc4b1f17ea4fd494aa10ff17fb377ce3e8d
Durgie246 <92335195+Durgie246@users.noreply.github.com>
2022-02-15 18:38:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function AddUsers()
+{
+    var player1_name = document.getElementById("player1_name_input").value; 
+    var player2_name = document.getElementById("player2_name_input").value; 
+
+    localStorage.setItem("Player1_name", player1_name);
+    localStorage.setItem("Player2_name", player2_name);
+
+    window.location = "mathquiz.html";
+
+}
